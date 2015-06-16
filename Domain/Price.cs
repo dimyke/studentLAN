a575dc2c0859f5bcd4128407b5a6ec4b37b46d08
dimyke <dimitrivantillo@gmail.com>
2015-06-16 20:35:13 +0200

    price + tournament

---
+++ Domain/Price.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class Price
+    {
+        public int PriceID { get; set; }
+        public string PriceName { get; set; }
+        public int PriceQuantity { get; set; }
+    }
+}
---
+++ Domain/Tournament.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class Tournament
+    {
+        public int TournamentID { get; set; }
+        public string TournamentName { get; set; }
+        public int MaxPlayers { get; set; }
+
+
+        //Foreign Key
+        public int GameID { get; set; }
+    }
+}
