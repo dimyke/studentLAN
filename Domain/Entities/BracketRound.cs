febdaee279e4fd04c7659c8b9f931564ca5e3bf3
dimyke <dimitrivantillo@gmail.com>
2015-06-16 22:03:25 +0200

    bracketround toegevoegd

---
+++ Domain/Entities/BracketRound.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    class BracketRound
+    {
+        public int BracketRoundID { get; set; }
+        public string WinnerID { get; set; }
+    }
+}
