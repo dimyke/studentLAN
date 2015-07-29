da8fd7fcc25f50fd828985fa396d6ef0b642d73a
dimyke <dimitrivantillo@gmail.com>
2015-07-29 21:26:18 +0200

    toevoegen

---
+++ Domain/Entities/Bestelling.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    class Bestelling:IOrder
+    {
+        public int BestellingID { get; set; }
+        public string GebruikerID { get; set; }
+        public double Bedrag { get; set; }
+    }
+}
---
+++ Domain/Entities/FoodDrinks.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    class FoodDrinks
+    {
+        public int ConsumptieID { get; set; }
+        public string Naam { get; set; }
+        public double prijs { get; set; }
+    }
+}
